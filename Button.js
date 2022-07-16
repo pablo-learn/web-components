f733a362f879ee798550cc6fd374f899435e7e98
Pablo Delfante <66787488+pablodelfante@users.noreply.github.com>
2022-07-16 20:12:55 +0000

    wip

--- Button.js
+++ Button.js
@@ -1,10 +1,43 @@
 class Button extends HTMLElement {
     constructor() {
         super();
-        var templateContent = document.getElementById('button-template').content;
-        var shadow = this.attachShadow({ mode: 'open' });
-        shadow.appendChild(templateContent.cloneNode(true));
-    }
+        
+        const template = document.createElement("template");
+        template.innerHTML = `
+        <header>
+          <script src="https://cdn.tailwindcss.com"></script>
+        </header>
+        <style>
+        @import "https://cdn.tailwindcss.com";
+         /*  button {
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            background-color: green;
+          } */
+        </style>
+        <button class="${this.getAttribute('class') || ''}">
+          <slot name="icon">
+            ❤️
+          </slot>
+          <slot>slot default</slot>
+        </button>
+        `;
+
+        var shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(template.content.cloneNode(true));
+
+        // on click
+        const button = this.shadowRoot.querySelector("button").addEventListener("click", this.handleClick);
+      }
+      handleClick(e) {
+        console.log('This button was clicked');
+      }
 }
 
-customElements.define('button-component', Button);
+customElements.define("button-component", Button);
