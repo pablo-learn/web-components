0d2969b0b3e8a113ec3a3dc3a549f6e3abdc75f1
Pabla <pablodelfante@vexels.com>
2025-11-27 21:39:41 -0300

    feat: Add dynamic page composition script and example `[posts]` directory structure for content generation.

---
+++ src/[posts]/index.js
@@ -0,0 +1,14 @@
+export const load = async () => {
+    return [
+        {
+            id: 1,
+            title: 'Post 1',
+            content: 'Content of post 1'
+        },
+        {
+            id: 2,
+            title: 'Post 2',
+            content: 'Content of post 2'
+        }
+    ]
+}
